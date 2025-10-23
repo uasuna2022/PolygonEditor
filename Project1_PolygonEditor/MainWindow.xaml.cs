24105ea280737c4a2f1336a040cdc13c0e6b64a9
uasuna2022 <max.gomanchuk@gmail.com>
2025-10-23 20:24:55 +0200

    Polygon class created and some fields and basic methods added, *xaml.cs files added with some example basic logic

---
+++ Project1_PolygonEditor/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project1_PolygonEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public DrawingAlgorithm CurrentSectionDrawingAlgorithm { get; private set; } = DrawingAlgorithm.Library;
+
+        private bool _isClosed = false;
+        private const double _vertexRadius = 4.0;
+        private readonly List<Point> _vertices = new();
+        private readonly List<Ellipse> _vertexDots = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CleanButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DrawingCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (_isClosed)
+                return;
+            Point position = e.GetPosition(DrawingCanvas);
+
+            _vertices.Add(position);
+
+            var dot = new Ellipse
+            {
+                Width = 2 * _vertexRadius,
+                Height = 2 * _vertexRadius,
+                Fill = Brushes.DodgerBlue,
+                Stroke = Brushes.Black,
+                StrokeThickness = 0.5
+            };
+            Canvas.SetLeft(dot, position.X - _vertexRadius);
+            Canvas.SetTop(dot, position.Y - _vertexRadius);
+
+            DrawingCanvas.Children.Add(dot);
+            _vertexDots.Add(dot);
+
+            e.Handled = true;
+        }
+    }
+}
---
+++ Project1_PolygonEditor/Models/Polygon.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shell;
+
+namespace Project1_PolygonEditor.Models
+{
+    public sealed class Polygon
+    {
+        private readonly Dictionary<int, Vertex> _vertices;
+        private readonly Dictionary<int, Edge> _edges;
+
+        private int _nextVertexIdx = 1;
+        private int _nextEdgeIdx = 1;
+
+        public bool IsClosed() => _edges.Count >= 3 && _edges.Count == _vertices.Count;
+        
+        public Vertex AddVertex(Point position)
+        {
+            Vertex newVertex = new Vertex(_nextVertexIdx, position);
+            _vertices.Add(newVertex.ID, newVertex);
+            _nextVertexIdx++;
+            return newVertex;
+        }
+        public Edge AddEdge(int v1ID, int v2ID)
+        {
+            if (!_vertices.ContainsKey(v1ID))
+                throw new ArgumentException($"Vertex nr. {v1ID} not found!");
+            if (!_vertices.ContainsKey(v2ID))
+                throw new ArgumentException($"Vertex nr. {v2ID} not found!");
+
+            Edge newEdge = new Edge(_nextEdgeIdx, v1ID, v2ID);
+            _edges.Add(newEdge.ID, newEdge);
+            _nextEdgeIdx++;
+            return newEdge;
+        }
+
+        
+    }
+}
