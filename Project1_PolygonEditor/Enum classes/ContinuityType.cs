66649fb0dbd1be993107b53c238fc9ef23db0e92
uasuna2022 <max.gomanchuk@gmail.com>
2025-10-23 19:17:18 +0200

    3 more enum classes added

---
+++ Project1_PolygonEditor/Enum classes/ContinuityType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1_PolygonEditor.Enum_classes
+{
+    public enum ContinuityType
+    {
+        G0,
+        G1,
+        C1
+    }
+}
