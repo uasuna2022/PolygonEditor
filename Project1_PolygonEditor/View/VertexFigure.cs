de02a810c4b547907b66ac4b93ef6b8f7d912940
uasuna2022 <max.gomanchuk@gmail.com>
2025-10-24 21:40:13 +0200

    VertexFigure (View) class added

---
+++ Project1_PolygonEditor/View/VertexFigure.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Project1_PolygonEditor.Models;
+
+namespace Project1_PolygonEditor.View
+{
+    public class VertexFigure
+    {
+        public Vertex Model { get; }
+        public double Radius { get; set; } = 4.0;
+        public Ellipse Shape { get; set; }
+
+        public VertexFigure(Vertex model)
+        {
+            Model = model;
+            Shape = new Ellipse
+            {
+                Width = 2 * Radius,
+                Height = 2 * Radius,
+                Fill = Brushes.DodgerBlue,
+                Stroke = Brushes.Black,
+                StrokeThickness = 1.0
+            };
+
+            Canvas.SetLeft(Shape, Model.Position.X - Radius);
+            Canvas.SetTop(Shape, Model.Position.Y - Radius);
+        }
+
+        public void DrawVertex(Canvas canvas)
+        {
+            canvas.Children.Add(Shape);
+        }
+
+        public void SyncToModel()
+        {
+            double x = Canvas.GetLeft(Shape) + Radius;
+            double y = Canvas.GetTop(Shape) + Radius;
+            Model.SetPosition(new Point(x, y));
+        }
+    }
+}
